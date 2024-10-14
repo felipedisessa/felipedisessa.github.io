8ac87bce3ebe3683b57b1ee914e1853e1b6dcc80
Felipe Di Sessa <felipetmds@hotmail.com>
2024-10-14 11:07:30 -0300

    wip

--- landpage-estudo/script.js
+++ landpage-estudo/script.js
@@ -5,12 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
         once: false      
     });
 
+    const clickLogo = document.querySelector('.logo');
+
+    clickLogo.addEventListener('click', function() {
+        window.location.href = '/landpage/landpage-estudo/';
+    });
+
     const menuToggle = document.querySelector('.menu-toggle');
     const navList = document.querySelector('.nav-list');
 
     // Adiciona evento de clique ao ícone de menu sanduíche
     menuToggle.addEventListener('click', function() {
-        // Alterna a classe 'active' no menu sanduíche e na lista de navegação
         menuToggle.classList.toggle('active');
         navList.classList.toggle('active');
     });
